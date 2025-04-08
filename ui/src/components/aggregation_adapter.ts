a66339f292ddf615a443fd22aa722875d6376488
Samuel Wu <wusamuel@google.com>
2025-04-08 12:23:41 -0700

    ui: Add wrapper to allow passing of custom AggregationPanel
    Add wrapper for createAggregationToTabAdaptor() that allows it to be
    passed a custom AggregationPanel.
    
    Bug: 409794084
    Signed-off-by: Samuel Wu <wusamuel@google.com>

--- ui/src/components/aggregation_adapter.ts
+++ ui/src/components/aggregation_adapter.ts
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import m from 'mithril';
-import {AggregationPanel} from './aggregation_panel';
+import {AggregationPanel, AggregationPanelAttrs} from './aggregation_panel';
 import {
   AreaSelection,
   AreaSelectionAggregator,
@@ -23,13 +23,19 @@ import {
 import {Trace} from '../public/trace';
 import {SelectionAggregationManager} from './selection_aggregation_manager';
 
+// Define a type for the expected props of the panel components so that a
+// generic AggregationPanel can be specificed as an argument to
+// createBaseAggregationToTabAdaptor()
+type PanelComponent = m.ComponentTypes<AggregationPanelAttrs>;
+
 /**
  * Creates an adapter that adapts an old style aggregation to a new area
  * selection sub-tab.
  */
-export function createAggregationToTabAdaptor(
+export function createBaseAggregationToTabAdaptor(
   trace: Trace,
   aggregator: AreaSelectionAggregator,
+  PanelComponent: PanelComponent
 ): AreaSelectionTab {
   const schemaSpecificity =
     (aggregator.schema && Object.keys(aggregator.schema).length) ?? 0;
@@ -58,7 +64,7 @@ export function createAggregationToTabAdaptor(
 
       return {
         isLoading: false,
-        content: m(AggregationPanel, {
+        content: m(PanelComponent, {
           data,
           trace,
           model: aggMan,
@@ -66,4 +72,11 @@ export function createAggregationToTabAdaptor(
       };
     },
   };
+}
+
+export function createAggregationToTabAdaptor(
+  trace: Trace,
+  aggregator: AreaSelectionAggregator
+): AreaSelectionTab {
+  return createBaseAggregationToTabAdaptor(trace, aggregator, AggregationPanel);
 }
